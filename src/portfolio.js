2cce3c8af60047178c0b9ee81e3871a5ad7487e5
Aaron Marshall <30453252+msmc19@users.noreply.github.com>
2024-10-31 20:41:43 -0400

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -179,7 +179,7 @@ const openSource = {
 // Some big projects you have worked on
 
 const bigProjects = {
-  title: "Big Projects 🛠️📊",
+  title: "Big Projects",
   subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
   projects: [
     {
