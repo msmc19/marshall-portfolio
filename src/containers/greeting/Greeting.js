4ffc904d01c415cc3337bddf52c6bb7d77b2523c
Aaron Marshall <30453252+msmc19@users.noreply.github.com>
2024-11-14 20:44:00 -0500

    Update Greeting.js

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -43,7 +43,7 @@ export default function Greeting() {
                 {greeting.resumeLink && (
                   <a
                     href={require("./aaronmarshallresume.pdf")}
-                    download="Resume.pdf"
+                    download="aaronmarshallresume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
